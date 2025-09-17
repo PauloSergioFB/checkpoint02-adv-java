4a10444053afe04fba1282b5ebe8be6239aeb2cd
PauloSergioFB <paulosergiofb2002@gmail.com>
2025-09-17 08:52:28 -0300

    Starting project

---
+++ src/main/java/br/com/fiap/tdsq/checkpoint02_adv_java/Checkpoint02AdvancedJavaApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.tdsq.checkpoint02_adv_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Checkpoint02AdvancedJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Checkpoint02AdvancedJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/tdsq/checkpoint02_adv_java/Checkpoint02AdvancedJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.tdsq.checkpoint02_adv_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Checkpoint02AdvancedJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
